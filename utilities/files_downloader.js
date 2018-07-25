8ce41cc475fdfff440a258f5f6e5ebddc70a5970
Anas <anas.ansal10@gmail.com>
2018-07-25 16:15:53 +0530

    adding async file

--- utilities/files_downloader.js
+++ utilities/files_downloader.js
@@ -4,22 +4,26 @@ const _ = require('underscore');
 const util = require('util');
 
 const config = {
-    prod:{
+    prod: {
         downloadDir: "/var/www/html/hott/laughingcolours/",
         dataFile: '../db/laughingcolorsposts'
     },
-    dev:{
+    dev: {
         downloadDir: "/Users/ansal/Downloads/",
         dataFile: '../db/laughingcolorsposts'
     }
 };
 const env = process.env.NODE_ENV || 'dev';
 
-const lastFileNumberInDownloadDir = () =>{
+const lastFileNumberInDownloadDir = () => {
     let files = fs.readdirSync(config[env].downloadDir);
-    let numberedFiles = _.filter(files, (f) => {return Number(f.split(".")[0])});
-    let sortedNames = _.sortBy(numberedFiles, (f) => {return f.toLowerCase()});
-    let lastName = sortedNames.length > 0 ? sortedNames[sortedNames.length-1] : "0.jpg";
+    let numberedFiles = _.filter(files, (f) => {
+        return Number(f.split(".")[0])
+    });
+    let sortedNames = _.sortBy(numberedFiles, (f) => {
+        return f.toLowerCase()
+    });
+    let lastName = sortedNames.length > 0 ? sortedNames[sortedNames.length - 1] : "0.jpg";
     return Number(lastName.split(".")[0]) || 0;
 };
 
@@ -38,7 +42,7 @@ const readDataFromUrlsFile = () => {
     return datas;  // [ url, caption ]
 };
 
-const downloadFilePromise = (url, destFilePath) =>{
+const downloadFilePromise = (url) => {
     return download(url);
 };
 
@@ -46,20 +50,27 @@ const execute = async () => {
     let startIndex = lastFileNumberInDownloadDir() + 1;
     let datas = readDataFromUrlsFile();
 
-    for (let i = 0 ; i < datas.length ; i++){
-        let destFilePath = util.format(config[env].downloadDir + "%s.jpg", startIndex);
-        let url = datas[i][0];
-        try {
-            let data = await downloadFilePromise(url, destFilePath);
-            fs.writeFileSync(destFilePath, data);
-            console.log("Downloaded "+ destFilePath);
-            startIndex ++;
-        }catch (e) {
-            console.log("Failed "+ destFilePath);
-            console.log(error);
-        }
+    await _.chunk(datas, 100).forEach(async (chunkData) => {
+        let promises = [];
+        chunkData.forEach((data) => {
+            let url = data[0];
+            promises.push(downloadFilePromise(url))
+        });
 
-    }
+        let resDatas = await Promise.all(promises);
+
+        resDatas.forEach((data) => {
+            let destFilePath = util.format(config[env].downloadDir + "%s.jpg", startIndex);
+            try {
+                fs.writeFileSync(destFilePath, data);
+                console.log("Downloaded " + destFilePath);
+                startIndex++;
+            } catch (e) {
+                console.log("Failed " + destFilePath);
+                console.log(error)
+            }
+        })
+    });
 };
 
 execute();
